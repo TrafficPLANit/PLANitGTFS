ba1dac733277b29d2ceaf9b8bccb651eb9a2aa75
Mark Raadsen <mark.raadsen@sydney.edu.au>
2023-06-01 17:36:45 +1000

    disable all but one test for CI debugging

--- src/test/java/org/goplanit/gtfs/test/GtfsToPlanitTest.java
+++ src/test/java/org/goplanit/gtfs/test/GtfsToPlanitTest.java
@@ -152,7 +152,7 @@ public class GtfsToPlanitTest {
    */
   @Disabled
   @Test
-  public void testGtfsIntermodalReaderWithoutPreExistingPlanitTransferZones() {
+  public void testGtfsIntermodalReaderWithoutPreExistingPlanitTransferZones() { 
 
     try {
       //String GTFS_FILES_DIR = Path.of(ResourceUtils.getResourceUri(GTFS_NSW_NO_SHAPES)).toAbsolutePath().toString();
